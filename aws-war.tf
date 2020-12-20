0996a447c64378aafc2ac5f3dc93ece18aea5683
lebedevds <blackbirdy87@gmail.com>
2020-12-20 17:16:56 +0300

    Update

--- aws-war.tf
+++ aws-war.tf
@@ -27,6 +27,7 @@ provisioner "remote-exec" {
   inline = [<<EOF
 sudo apt-get update
 sudo apt-get install git default-jdk maven -y
+git clone https://github.com/lebedevds/test-webapp.git
 maven package
 EOF
 ]
